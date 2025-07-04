85941f82c76a3f94e5d6708e8bada208deb4d83a
Vikash Rathod <vikeyrathod007@gmail.com>
2025-07-05 03:46:19 +0530

    Fix manifest plugin: use PNG icon instead of SVG to resolve deployment issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.svg',
+        icon: 'src/images/favicons/android-icon-192x192.png',
       },
     },
     `gatsby-plugin-offline`,
